280cc35d676f764d0546cd370c88320a5984cc9f
Mikita Iwanowski <info@slnt-opp.xyz>
2020-05-08 16:04:31 +0200

    env

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3.8
-from os import getenv
-from app import app
-
 from dotenv import load_dotenv
 load_dotenv()
+from os import getenv
+
+from app import app
 
 if __name__ == "__main__":
     app.run(host=getenv('HOST'), port=getenv('PORT'))
